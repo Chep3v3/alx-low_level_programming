2dcbb9b188131b5cda639b17a96b92c1f2674e0e
yora3vs3 <99631524+yora3vs3@users.noreply.github.com>
2023-09-30 19:47:12 +0300

    location of data in memory

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,15 +1,17 @@
-#include"main.h"
+#include "main.h"
+
 /**
- * _pow_recursion -returns the value of x raised to the power of y
+ * _pow_recursion - returns the value of x raised to the power of y
  * @x: base number
  * @y: exponent
- * 
- * Return:x^y or -1 if y<0
+ *
+ * Return: x ^ y or -1 if y < 0
  */
-int _pow_recursions(int x, int y)
+int _pow_recursion(int x, int y)
 {
-    if (y<0)
-       return (-1);
-    if (y==0);
-       return ( x* _pow_recursions(x,y -1));
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
 }
